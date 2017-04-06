6ebd37c9568370f9f92d1fceee007c4b16955b68
Les Aker <me@lesaker.org>
2017-04-05 23:31:34 -0400

    fix linting

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,18 +8,18 @@ class bettertouchtool (
   package { 'bettertouchtool-halyard':
     provider => 'brewcask',
     require  => Homebrew::Tap['halyard/casks']
-  } ->
-  osx_default { 'Enable launch on boot for BetterTouchTool':
+  }
+  -> osx_default { 'Enable launch on boot for BetterTouchTool':
     domain => 'com.hegenberg.BetterTouchTool',
     key    => 'launchOnStartup',
     value  => 1,
     type   => 'integer'
-  } ->
-  osx_login_item { 'BetterTouchTool':
+  }
+  -> osx_login_item { 'BetterTouchTool':
     hidden => true,
     path   => $install_path
-  } ~>
-  exec { 'launch bettertouchtool':
+  }
+  ~> exec { 'launch bettertouchtool':
     command     => "/usr/bin/open ${install_path}",
     refreshonly => true,
   }
